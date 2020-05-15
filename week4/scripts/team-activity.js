0a99d19094d364563a7e05ba9cf6d6b56601305d
jeremybgriffiths <gri18025@byui.edu>
2020-05-14 21:14:47 -0600

    Cleaned up event listeners

--- week4/scripts/team-activity.js
+++ week4/scripts/team-activity.js
@@ -1,14 +1,4 @@
-document.getElementById('r1c1').addEventListener('touchend', fillBoard);
-document.getElementById('r1c2').addEventListener('touchend', fillBoard);
-document.getElementById('r1c3').addEventListener('touchend', fillBoard);
-document.getElementById('r2c1').addEventListener('touchend', fillBoard);
-document.getElementById('r2c2').addEventListener('touchend', fillBoard);
-document.getElementById('r2c3').addEventListener('touchend', fillBoard);
-document.getElementById('r3c1').addEventListener('touchend', fillBoard);
-document.getElementById('r3c2').addEventListener('touchend', fillBoard);
-document.getElementById('r3c3').addEventListener('touchend', fillBoard);
-document.getElementById('reset').addEventListener('click', reset);
-
+const board = document.querySelector('.board');
 let turnX = true;
 
 function fillBoard(event) {
@@ -20,8 +10,6 @@ function getTurn() {
     return turnX ? 'X' : 'O';
 }
 
-const board = document.querySelector('.board')
-
 function reset() {
     for (let i = 0; i < board.rows.length; i++) {
         let row = board.rows[i];
@@ -30,3 +18,6 @@ function reset() {
         }
     }
 }
+
+board.addEventListener('touchend', fillBoard);
+document.getElementById('reset').addEventListener('click', reset);
